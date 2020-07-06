3f96d39e15e4a71872268089d8b41944e412b3f5
Gurpal <gurpal.gwebs@gmail.com>
2020-07-06 08:53:13 +0530

    project setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var gulpIf = require('gulp-if');
 var rename = require('gulp-rename');
@@ -6,13 +8,18 @@ var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 
+gulp.task('js', function(){
+    gulp.src('node_modules/bootstrap/dist/js/**.*')
+        .pipe(gulp.dest('js/'));
+});
+
 gulp.task('sass', function() {
   return gulp.src('css/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('css'))
-})
+});
 
 gulp.task('stylescompress', ['sass'], function() {
     gulp.src("css/**/style.css")
@@ -21,23 +28,32 @@ gulp.task('stylescompress', ['sass'], function() {
     .pipe(gulp.dest('css/'));
 });
 
+gulp.task('bootstrapcompress', ['sass'], function() {
+    gulp.src("css/**/bootstrap.css")
+    .pipe(rename('bootstrap.min.css'))
+    .pipe(gulpIf('*bootstrap.min.css', cssnano()))
+    .pipe(gulp.dest('css/'));
+});
+
 
 gulp.task('watch', function(){
-  gulp.watch('css/**/*.scss', ['sass','stylescompress']);
-})
+  gulp.watch('css/**/*.scss', ['sass','stylescompress','bootstrapcompress']);
+});
 
 
 // Build Sequences
 // ---------------
 
 gulp.task('default', function(callback) {
-    runSequence(['sass','stylescompress'], 'watch'
+    runSequence(['sass','stylescompress','bootstrapcompress','js'], 'watch'
     )
-})
+});
 
 gulp.task('build', function(callback) {
     runSequence(
         'sass',
-        'stylescompress'
+        'stylescompress',
+        'bootstrapcompress',
+        'js'
     )
-})
+});
