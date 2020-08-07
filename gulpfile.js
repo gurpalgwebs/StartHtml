945e9b9feaf3a96dcafb6540156eb9ad73ec2936
Gurpal <gurpal.gwebs@gmail.com>
2020-08-07 15:08:59 +0530

    change node version

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,58 @@
+
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var gulpIf = require('gulp-if');
 var rename = require('gulp-rename');
-var cssnano = require('gulp-cssnano');
-var runSequence = require('run-sequence');
+var cssnano = require('cssnano');
 var sourcemaps = require('gulp-sourcemaps');
-
-gulp.task('js', function(){
-    gulp.src('node_modules/bootstrap/dist/js/**.*')
-        .pipe(gulp.dest('js/'));
-});
-
-gulp.task('sass', function() {
-  return gulp.src('css/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('css'))
-});
-
-gulp.task('stylescompress', ['sass'], function() {
-    gulp.src("css/**/style.css")
-    .pipe(rename('style.min.css'))
-    .pipe(gulpIf('*style.min.css', cssnano()))
-    .pipe(gulp.dest('css/'));
-});
-
-gulp.task('bootstrapcompress', ['sass'], function() {
-    gulp.src("css/**/bootstrap.css")
-    .pipe(rename('bootstrap.min.css'))
-    .pipe(gulpIf('*bootstrap.min.css', cssnano()))
-    .pipe(gulp.dest('css/'));
-});
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 
-gulp.task('watch', function(){
-  gulp.watch('css/**/*.scss', ['sass','stylescompress','bootstrapcompress']);
-});
+
+var paths = {
+    styles: {
+        src: 'css/**/*.scss',
+        dest: 'css'
+    },
+    scripts: {
+        src: 'node_modules/bootstrap/dist/js/**/*.js',
+        dest: 'js/'
+    }
+};
+
+function compressTask(){
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(postcss([ autoprefixer(), cssnano()]))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scssTask(){
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.styles.dest));
+}
 
 
-// Build Sequences
-// ---------------
+function jsTask(){
+    return gulp.src(paths.scripts.src)
+        //.pipe(concat('all.js'))
+        //.pipe(uglify())
+        .pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task('default', function(callback) {
-    runSequence(['sass','stylescompress','bootstrapcompress','js'], 'watch'
-    )
-});
+function watch(){
+    gulp.watch(
+        [paths.styles.src, paths.scripts.src],
+        gulp.parallel(scssTask, compressTask)
+    );
+}
 
-gulp.task('build', function(callback) {
-    runSequence(
-        'sass',
-        'stylescompress',
-        'bootstrapcompress',
-        'js'
-    )
-});
+exports.default = gulp.series(
+    gulp.parallel(scssTask, compressTask ,jsTask ), watch);
